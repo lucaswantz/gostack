cad33cc185820aa4ae833fa74293a82b3e3ded70
Lucas W. da Motta <lucaswantz@hotmail.com>
2020-08-16 23:40:49 -0300

    Backend: Conectada a apĺicação com o banco de dados

--- projetos/primeiro-projeto-node/src/routes/appointments.routes.ts
+++ projetos/primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../service/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
 // Rota: Receber a requisição, chamar outro arquivo para tratar a requisição,
 // devolver uma resposta
@@ -14,24 +14,23 @@ const appointmentsRepository = new AppointmentsRepository();
 // DTO: Data Transfer Object
 
 // GET http://localhost:3333/appointments
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
 // POST http://localhost:3333/appointments
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService(
-      appointmentsRepository,
-    );
+    const createAppointment = new CreateAppointmentService();
 
-    const appointment = createAppointment.execute({
+    const appointment = await createAppointment.execute({
       date: parsedDate,
       provider,
     });
