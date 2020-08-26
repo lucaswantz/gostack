aca98a7097a3778604d817bafbe9f7223c9d5b51
Lucas W. da Motta <lucaswantz@hotmail.com>
2020-08-25 23:09:06 -0300

    Frontend Git: Navagando entre rotas

--- projetos/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ projetos/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
@@ -67,7 +68,7 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="repository">
+          <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
             <img src={repository.owner.avatar_url} alt={repository.owner.login} />
             <div>
               <strong>{repository.full_name}</strong>
@@ -75,7 +76,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
