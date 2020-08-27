30e4a2d65b2f7be9bf3f848839edf73e1ec5f767
Lucas W. da Motta <lucaswantz@hotmail.com>
2020-08-27 01:38:41 -0300

    Frontend Git: Finalizada aplicação

--- projetos/primeiro-projeto-react/src/pages/Repository/index.tsx
+++ projetos/primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 import { Header, RepositoryInfo, Issues } from './styles';
@@ -9,8 +10,60 @@ interface RepositoryParams {
   repository: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
+
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
   const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    // OPCAO 01 - Executa as duas requisições ao mesmo tempo
+    // Foi utilizada essa opção, mas o ideal seria Promise.all
+    api.get(`repos/${params.repository}`).then(response => {
+      setRepository(response.data);
+    });
+
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      setIssues(response.data);
+    });
+
+    // // OPCAO 02 - Executa um de cada vez, a segunda linha espera a primeira linha concluir
+    // async function loadData(): Promise<Void> {
+    //   // const repository = await api.get(`repos/${params.repository}`);
+    //   // const issues = await api.get(`repos/${params.repository}/issues`);
+
+    //   // Assim executa as duas no mesmo momento
+    //   const [repository, issues] = await Promise.all([
+    //     api.get(`repos/${params.repository}`),
+    //     api.get(`repos/${params.repository}/issues`),
+    //   ]);
+
+    //   console.log(repository);
+    //   console.log(issues);
+    // }
+    // loadData();
+  }, [params.repository]);
 
   return (
     <>
@@ -22,39 +75,43 @@ const Repository: React.FC = () => {
         </Link>
       </Header>
 
-      <RepositoryInfo>
-        <header>
-          <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
-          <div>
-            <strong>facebook/react</strong>
-            <p>Descrição do repositório</p>
-          </div>
-        </header>
-        <ul>
-          <li>
-            <strong>1808</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>48</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>67</strong>
-            <span>Issues abertas</span>
-          </li>
-        </ul>
-      </RepositoryInfo>
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
 
       <Issues>
-        <Link to="/">
-          <div>
-            <strong>Qualquer coisa</strong>
-            <p>Descrição de qualquer coisa</p>
-          </div>
+        {issues.map(issue => (
+          <a key={issue.id} href={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </Link>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Issues>
     </>
   );
