9010eeae2f765958637b813c0d9f122c19c7056b
Lucas W. da Motta <lucaswantz@hotmail.com>
2020-08-29 16:26:48 -0300

    GoBarber Web: Criado projeto e tela de login

---
+++ projetos/gobarber-web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Signin from './pages/Signin';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <Signin />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
