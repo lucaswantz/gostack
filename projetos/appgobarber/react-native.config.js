3be4c0112a1b6211943c6bbb685163417f14dfaf
Lucas W. da Motta <lucaswantz@hotmail.com>
2020-09-06 19:03:12 -0300

    AppGoBarber: Fontes personalizadas

---
+++ projetos/appgobarber/react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: [
+    './assets/fonts'
+  ]
+}
+
+// Linkar os assets para serem usado no projeto, após usar
+// yarn react-native link
