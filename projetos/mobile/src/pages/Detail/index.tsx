2c9f204628cb625ba56a6851f9df094c156ed159
Lucas W. da Motta <lucaswantz@hotmail.com>
2020-09-17 15:58:23 -0300

    Tiny Class

---
+++ projetos/mobile/src/pages/Detail/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Detail: React.FC = () => {
+    return (
+        <View style={style.view} >
+            <Text>Detail</Text>
+        </View>
+    );
+}
+
+export default Detail;
+
+const style = StyleSheet.create({
+    view: {},
+    container: {},
+    text: {}
+})
---
+++ projetos/mobile/src/routes/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from '../pages/Home';
+import Detail from '../pages/Detail';
+
+const Stack = createStackNavigator();
+
+const Route: React.FC = () => (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Detail" component={Detail} />
+        </Stack.Navigator>
+    </NavigationContainer>
+
+);
+
+export default Route;
